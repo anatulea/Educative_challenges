3f2c23af326c9c6ed46eee24a49dc9d2232d3933
Ana Tulea <anatulea14@gmail.com>
2021-02-08 22:48:55 -0800

    search_recursive

--- Linked Lists/02_search.py
+++ Linked Lists/02_search.py
@@ -78,6 +78,35 @@ def search(lst, value):
                 curr_node = curr_node.next_element
     return False
 
+'''
+Educative solution:
+def search(lst, value):
+
+    # Start from first element
+    current_node = lst.get_head()
+
+    # Traverse the list till you reach end
+    while current_node:
+        if current_node.data == value:
+            return True  # value found
+        current_node = current_node.next_element
+
+    return False  # value not found
+
+'''
+
+def search_recursive(node, value):
+
+    # Base case
+    if(not node):
+        return False  # value not found
+
+    # check if the node's data matches our value
+    if(node.data is value):
+        return True  # value found
+
+    # Recursive call to next node in the list
+    return search_recursive(node.next_element, value)
 
 lst=LinkedList()
 lst.print_list() # List is empty!
